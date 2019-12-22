26234d331b5aafbf6e3f4b024e44abd67b605594
Heedx <ravine4675@gmail.com>
2019-12-21 20:12:31 -0500

    added util files aand the drive function

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+
+package frc.robot;
+
+//file to hold constants used around the robot.
+
+public class Constants{
+    //drive ports
+    public final static int kDriveLeftMaster = 1;
+    public final static int kDriveLeftSlave = 16;
+    public final static int kDriveRightMaster = 14;
+    public final static int kDriveRightSlave = 15;
+
+}
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Constants;
+
+/**
+ * The drive subsystem.  Controls the drivetrain on teh robot.
+ */
+public class Drive extends Subsystem {
+
+    //motors
+    CANSparkMax mLeftMaster = new CANSparkMax(Constants.kDriveLeftMaster, MotorType.kBrushless);
+    CANSparkMax mRightMaster = new CANSparkMax(Constants.kDriveRightMaster, MotorType.kBrushless);
+    CANSparkMax mLeftSlave = new CANSparkMax(Constants.kDriveLeftSlave, MotorType.kBrushless);
+    CANSparkMax mRightSlave = new CANSparkMax(Constants.kDriveRightSlave, MotorType.kBrushless);
+    
+
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
